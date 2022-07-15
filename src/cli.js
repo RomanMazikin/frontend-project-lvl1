536fdf16d2e1f6551aa200629f2f2522d4e7c08d
RomanMazikin <roman.mazikin@gmail.com>
2022-07-15 20:38:29 +0300

    шаг3

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const name = readlineSync.question('May I have your name?');
+
+const greeting = () => {
+    console.log(`Hello, ${name}!`)
+};
+
+export default greeting;
