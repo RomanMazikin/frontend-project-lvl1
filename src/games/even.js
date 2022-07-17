2f72c655e01111f03072b53d42149e15a9b69ef6
RomanMazikin <roman.mazikin@gmail.com>
2022-07-17 15:50:02 +0300

    update

---
+++ src/games/even.js
@@ -0,0 +1,17 @@
+import randomNumber from '../random-number.js';
+import start from '../index.js';
+
+
+const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
+const isEven = (number) => (number % 2 === 0);
+
+const taskResult = () => {
+    const number = randomNumber(0, 100);
+    const question = number;
+    const correctAnswer = isEven(question) ? 'yes' : 'no';
+    return [question, correctAnswer];
+};
+
+const startEven = () => start(rules, taskResult);
+
+export default startEven;
